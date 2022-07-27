d8c8f27d80f444bb13a5b577949f5a6c1ad929ae
Bolabouh <bolabouh0@gmail.com>
2022-07-27 19:59:46 -0400

    Tasks completed 2

--- src/component/todos.js
+++ src/component/todos.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../component/todos.css";
 import { Card, Grid, ListItemButton, ListItemText, Checkbox} from "@mui/material";
 
-// 1. This component formats and returns the list of todos.
+// 1. This component formats and returns the list of todos. 2
 // 2. Treat the question mark like an if statement.
 // If the todos array has items in the list [todos.length], we want to return the list
 // Else, return a message saying "You have no todo's left"
